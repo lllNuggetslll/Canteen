ca8f5a4fca47619c0378e146777c53ae7f04d3e4
David Nguyen <davidnguyen21@gmail.com>
2016-04-25 03:06:04 +0900

    Creating tasks now updates the Task List

--- public/client/task-form/taskForm.js
+++ public/client/task-form/taskForm.js
@@ -5,23 +5,30 @@ angular.module('canteen.taskForm', [])
   '$scope',
   'formFactory',
   'trip',
-  function ($scope, formFactory, trip) {
+  'taskHolder',
+
+  function($scope, formFactory, trip, taskHolder) {
     $scope.taskForm = {
       statusCode: 0
     };
 
-    $scope.createTask = function () {
-      formFactory.submitTask($scope.taskForm, $scope.trip._id);
-      console.log($scope.taskForm)
+    $scope.createTask = function() {
+      formFactory.submitTask($scope.taskForm, $scope.trip._id)
+        .then(function(data) {
+          if (data) {
+            taskHolder.refreshTasks();
+          }
+        });
       $scope.taskForm = {
         statusCode: 0
       };
       // $scope.bullets = [];
 
       trip.getTrip($scope.trip._id)
-      .then(function (trip) {
-        $scope.$parent.trip = trip;
-      })
+        .then(function(trip) {
+          $scope.$parent.trip = trip;
+        })
     };
+
   },
 ]);
